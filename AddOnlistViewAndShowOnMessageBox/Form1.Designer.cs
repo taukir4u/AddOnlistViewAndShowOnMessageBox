8bd5b13a84a77ec81adc171d49ec93d2363f300d
taukir4u <taukir4u@gmail.com>
2014-12-26 18:55:47 +0600

    C#
    Signed-off-by: taukir4u <taukir4u@gmail.com>

---
+++ AddOnlistViewAndShowOnMessageBox/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace AddOnlistViewAndShowOnMessageBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.takeValueText = new System.Windows.Forms.TextBox();
+            this.showListView = new System.Windows.Forms.ListView();
+            this.addButton = new System.Windows.Forms.Button();
+            this.showButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // takeValueText
+            // 
+            this.takeValueText.Location = new System.Drawing.Point(66, 15);
+            this.takeValueText.Name = "takeValueText";
+            this.takeValueText.Size = new System.Drawing.Size(126, 20);
+            this.takeValueText.TabIndex = 0;
+            // 
+            // showListView
+            // 
+            this.showListView.Location = new System.Drawing.Point(136, 110);
+            this.showListView.Name = "showListView";
+            this.showListView.Size = new System.Drawing.Size(121, 97);
+            this.showListView.TabIndex = 1;
+            this.showListView.UseCompatibleStateImageBehavior = false;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(238, 12);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 2;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(163, 243);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(75, 23);
+            this.showButton.TabIndex = 3;
+            this.showButton.Text = "Show";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 369);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.showListView);
+            this.Controls.Add(this.takeValueText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox takeValueText;
+        private System.Windows.Forms.ListView showListView;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button showButton;
+    }
+}
+
---
+++ AddOnlistViewAndShowOnMessageBox/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AddOnlistViewAndShowOnMessageBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int value;
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            value = Convert.ToInt16(takeValueText.Text);
+            for (int i =0 ; i <= value; i++)
+            {
+                showListView.Items.Add(i.ToString());
+            }
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            var selectedItems = showListView.SelectedItems;
+            if (selectedItems.Count > 0)
+            {
+                // Display text of first item selected.
+                MessageBox.Show(selectedItems[0].Text);
+            }
+
+        }
+
+
+    }
+}
